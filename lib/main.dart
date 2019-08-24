8a0a3059982161b865b7f1012fe7d0a1b499aacb
Joseph <dabigjoe6@gmail.com>
2019-08-24 13:50:10 +0100

    Create Stateless view for first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp( RootApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return  MaterialApp(
+      title: 'To-Do',
+      home:  Scaffold(
+        // backgroundColor: const Color.fromRGBO(36, 36, 38, 0.0),
+        body: SafeArea(
+          child: Column(
+              children: <Widget>[
+              //User profile
+              ProfileWidget(),
+              Divider(),
+              Expanded (
+                child: TodoWidget()
+              )
+              ],
+              mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+          )
+        )
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );// theme: ThemeData.dark()
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding: EdgeInsets.only(top: 20, right: 15, left: 15),
+      child: Row(
+        children: <Widget>[
+           DisplayUserWidget(),
+           ActionWidget()
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DisplayUserWidget extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return  Column(
+      children: <Widget>[
+        //user avatar
+         CircleAvatar(
+          backgroundImage: AssetImage('assets/display_picture.jpg'),
+          radius: 32
+        ),
+        //user full name
+        Container(
+          margin: EdgeInsets.only(bottom: 5, top: 12),
+          child: Text(
+            'Joseph Olabisi', 
+            style: TextStyle(
+              // color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18
+            )
+          )
+        ),
+        //user email
+         Text(
+          'josepholabisi6000@gmail.com',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 12
+          )
+        )
+      ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ActionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.only(bottom: 30),
+          child: IconButton(
+            padding: EdgeInsets.only(top: 0),
+            icon: Icon(Icons.search, size: 43),
+            tooltip: 'Search',
+            onPressed: () {
+              //Do nothing for now
+            }
+          )
+        ),
+        Icon(Icons.arrow_drop_down),
+        
+      ],
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class TodoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  ListView(
+        children: <Widget>[
+          TodoListWidget(Icons.lightbulb_outline, 'My Day', '2'),
+          TodoListWidget(Icons.star_border, 'Important', '1'),
+          TodoListWidget(Icons.calendar_today, 'Planned', ''),
+          TodoListWidget(Icons.person_pin, 'Assigned to Me', ''),
+          TodoListWidget(Icons.home, 'Task', '17'),
+          Divider(),
+          TodoListWidget(Icons.format_list_bulleted, 'Ideas', '23')
+        ]
+    );
+  }
+}
+
+class TodoListWidget extends StatelessWidget {
+
+  IconData icon;
+  String title;
+  String trailing;
+
+  TodoListWidget(IconData icon, String title, String trailing) {
+    this.icon = icon;
+    this.title = title;
+    this.trailing = trailing;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return  ListTile(
+      leading:  Icon(this.icon),
+      title:  Text(this.title, style: TextStyle( fontSize: 15)),
+      trailing:  Text(this.trailing),
+      contentPadding: EdgeInsets.only(right: 15, left: 15),
+      dense: true
     );
   }
 }
