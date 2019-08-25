bda800130bf9fc5ddc42f61d574449d621d9ac86
Joseph <dabigjoe6@gmail.com>
2019-08-25 01:08:00 +0100

    Add SearchScreen static visuals

--- lib/screens/searchScreen/SearchScreen.dart
+++ lib/screens/searchScreen/SearchScreen.dart
@@ -3,6 +3,52 @@ import 'package:flutter/material.dart';
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Search Screen'),);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            //Do nothing for now
+            Navigator.pop(context);
+          }
+        ),
+        title: TextField(
+          autofocus: true,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Search'
+          )
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.mic),
+            onPressed: () {
+              //Do nothing for now
+            }
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              //Do nothing for now
+            }
+          )
+        ]
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/rocket.jpg', scale: 3),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Text('What would you like to find? You can search within tasks, steps and notes', textAlign: TextAlign.center, style: TextStyle(fontSize: 17))
+              )
+            ]
+          )
+        )
+      )
+    );
   }
 }
---
+++ lib/screens/todoListScreen/TodoListScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Text('TodoListScreen')
+    );
+  }
+}
