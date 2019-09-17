d5a80b3f470e17f69acf9bee92befefed4bfba54
Joseph <dabigjoe6@gmail.com>
2019-09-17 01:02:44 +0100

    Change the bottom stuff ui in TodoScreen

--- lib/screens/todoScreen/TodoScreen.dart
+++ lib/screens/todoScreen/TodoScreen.dart
@@ -14,125 +14,137 @@ class TodoScreen extends StatelessWidget {
                 IconButton(icon: Icon(Icons.arrow_back), onPressed: () {})),
         body: Center(
             child: SingleChildScrollView(
+                child: 
+          Column(children: <Widget>[
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.check_box_outline_blank,
+                            size: 35, color: Colors.white),
+                        onPressed: () {}),
+                    Text('Task you set out to do',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white)),
+                    IconButton(
+                        icon: Icon(Icons.star_border,
+                            size: 35, color: Colors.white),
+                        onPressed: () {}),
+                  ],
+                )),
+            Card(
+                child: Divider(color: Colors.transparent, height: 0.1),
+                elevation: 3),
+            FlatButton(
+                child: Row(children: <Widget>[
+                  Container(
+                      child: Icon(Icons.add, color: Colors.white),
+                      margin: EdgeInsets.only(right: 20)),
+                  Text('Add step', style: TextStyle(color: Colors.white))
+                ]),
+                onPressed: () {}),
+            Divider(color: Colors.grey[800]),
+            Card(
+                color: Colors.grey[900],
+                child: FlatButton(
+                    child: Row(children: <Widget>[
+                      Container(
+                          child: Icon(Icons.brightness_7, color: Colors.white),
+                          margin: EdgeInsets.only(right: 20)),
+                      Text('Add to my day',
+                          style: TextStyle(color: Colors.white, fontSize: 17))
+                    ]),
+                    onPressed: () {})),
+            Card(
+                color: Colors.grey[900],
                 child: Column(children: <Widget>[
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.check_box_outline_blank,
-                          size: 35, color: Colors.white),
+                  FlatButton(
+                      child: Row(children: <Widget>[
+                        Container(
+                            child: Icon(Icons.alarm, color: Colors.white),
+                            margin: EdgeInsets.only(right: 20)),
+                        Text('Remind me',
+                            style: TextStyle(color: Colors.white, fontSize: 17))
+                      ]),
                       onPressed: () {}),
-                  Text('Task you set out to do',
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white)),
-                  IconButton(
-                      icon: Icon(Icons.star_border,
-                          size: 35, color: Colors.white),
+                  Divider(color: Colors.black, indent: 55),
+                  FlatButton(
+                      child: Row(children: <Widget>[
+                        Container(
+                            child:
+                                Icon(Icons.calendar_today, color: Colors.white),
+                            margin: EdgeInsets.only(right: 20)),
+                        Text('Add due date',
+                            style: TextStyle(color: Colors.white, fontSize: 17))
+                      ]),
                       onPressed: () {}),
-                ],
-              )),
-          Card(
-              child: Divider(color: Colors.transparent, height: 0.1),
-              elevation: 3),
-          FlatButton(
-              child: Row(children: <Widget>[
-                Container(
-                    child: Icon(Icons.add, color: Colors.white),
-                    margin: EdgeInsets.only(right: 20)),
-                Text('Add step', style: TextStyle(color: Colors.white))
-              ]),
-              onPressed: () {}),
-          Divider(color: Colors.grey[800]),
-          Card(
-              color: Colors.grey[900],
-              child: FlatButton(
-                  child: Row(children: <Widget>[
+                  Divider(color: Colors.black, indent: 55),
+                  FlatButton(
+                      child: Row(children: <Widget>[
+                        Container(
+                            child: Icon(Icons.autorenew, color: Colors.white),
+                            margin: EdgeInsets.only(right: 20)),
+                        Text('Repeat',
+                            style: TextStyle(color: Colors.white, fontSize: 17))
+                      ]),
+                      onPressed: () {}),
+                ])),
+            Card(
+                color: Colors.grey[900],
+                child: FlatButton(
+                    child: Row(children: <Widget>[
+                      Container(
+                          child: Icon(Icons.file_upload, color: Colors.white),
+                          margin: EdgeInsets.only(right: 20)),
+                      Text('Add file',
+                          style: TextStyle(color: Colors.white, fontSize: 17))
+                    ]),
+                    onPressed: () {})),
+            Container(
+                margin: EdgeInsets.only(top: 10),
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.grey[900]),
+                    child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                                borderSide: BorderSide.none),
+                            contentPadding: EdgeInsets.all(10),
+                            hintText: 'Add note',
+                            hintStyle: TextStyle(color: Colors.white)),
+                        autocorrect: true,
+                        cursorColor: Colors.white,
+                        expands: true,
+                        maxLines: null,
+                        minLines: null),
+                    height: 110)),
                     Container(
-                        child: Icon(Icons.brightness_7, color: Colors.white),
-                        margin: EdgeInsets.only(right: 20)),
-                    Text('Add to my day',
-                        style: TextStyle(color: Colors.white, fontSize: 17))
-                  ]),
-                  onPressed: () {})),
-          Card(
-              color: Colors.grey[900],
-              child: Column(children: <Widget>[
-                FlatButton(
-                    child: Row(children: <Widget>[
+                      margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text('Created on Tue, Aug, 20',
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
                       Container(
-                          child: Icon(Icons.alarm, color: Colors.white),
-                          margin: EdgeInsets.only(right: 20)),
-                      Text('Remind me',
-                          style: TextStyle(color: Colors.white, fontSize: 17))
-                    ]),
-                    onPressed: () {}),
-                Divider(color: Colors.black, indent: 55),
-                FlatButton(
-                    child: Row(children: <Widget>[
-                      Container(
-                          child:
-                              Icon(Icons.calendar_today, color: Colors.white),
-                          margin: EdgeInsets.only(right: 20)),
-                      Text('Add due date',
-                          style: TextStyle(color: Colors.white, fontSize: 17))
-                    ]),
-                    onPressed: () {}),
-                Divider(color: Colors.black, indent: 55),
-                FlatButton(
-                    child: Row(children: <Widget>[
-                      Container(
-                          child: Icon(Icons.autorenew, color: Colors.white),
-                          margin: EdgeInsets.only(right: 20)),
-                      Text('Repeat',
-                          style: TextStyle(color: Colors.white, fontSize: 17))
-                    ]),
-                    onPressed: () {}),
-              ])),
-          Card(
-              color: Colors.grey[900],
-              child: FlatButton(
-                  child: Row(children: <Widget>[
-                    Container(
-                        child: Icon(Icons.file_upload, color: Colors.white),
-                        margin: EdgeInsets.only(right: 20)),
-                    Text('Add file',
-                        style: TextStyle(color: Colors.white, fontSize: 17))
-                  ]),
-                  onPressed: () {})),
-          Container(
-              margin: EdgeInsets.only(top: 10),
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.grey[900]),
-                  child: TextField(
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                          border:
-                              UnderlineInputBorder(borderSide: BorderSide.none),
-                          contentPadding: EdgeInsets.all(10),
-                          hintText: 'Add note',
-                          hintStyle: TextStyle(color: Colors.white)),
-                      autocorrect: true,
-                      cursorColor: Colors.white,
-                      expands: true,
-                      maxLines: null,
-                      minLines: null),
-                  height: 110)),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Created on Tue, Aug, 20'),
-                  IconButton(icon: Icon(Icons.delete), onPressed: () {})
-                ],
-              ))
-        ]))));
+                        height: 25,
+                        width: 25,
+                        child: IconButton(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(0),
+                          icon: Icon(Icons.delete, color: Colors.white),
+                          onPressed: () {}))
+                    ],
+                  ))
+          ]),
+         )));
   }
 }
